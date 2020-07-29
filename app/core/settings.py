0f5bb88362c960a45baf18ebb9c855ebdda83e27
Marco Orfei <marcoasrorfei@gmail.com>
2020-07-29 11:56:30 +0200

    Update settings

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,19 +1,22 @@
 class Settings:
+    """
+    Default properties
+    """
 
-    USERNAM = "g.cardone"
-    PASSWORDSTR = "Giulio%102"
+    USERNAME = "g.cardone"
+    PASSWORD = "Giulio%102"
     PRIVATE_AREA = "https://www.uniecampus.it/area-riservata"
     SIGNIN_BUTTON_NAME = "_eventId_proceed"
-    GOTOPAGEONE = "https://www.uniecampus.it/area-riservata/lezioni-e-laboratori"
-    GOTOPAGETWO = (
+    GO_TO_PAGE_ONE = "https://www.uniecampus.it/area-riservata/lezioni-e-laboratori"
+    GO_TO_PAGE_TWO = (
         "https://www.uniecampus.it/area-riservata/lezioni-e-laboratori/vai-a-studiare"
     )
-    FRAMENAME = "iframe"
+    FRAME_NAME = "iframe"
     FIRST_PART_OF_NAME = "ctl00_ContentPlaceHolder1_d_dettaglio_ctl0"
     END_PART_OF_NAME = "_button1"
-    ELEMENTNAME = "ctl00_ContentPlaceHolder1_b_prossima"
-    ELEMENTBYXPATH = "//table[@id='ctl00_ContentPlaceHolder1_d_dettaglio']/tbody/tr"
-    ERRORBOTTOM = "ctl00_ContentPlaceHolder1_pbOkErrScorm"
+    ELEMENT_NAME = "ctl00_ContentPlaceHolder1_b_prossima"
+    ELEMENT_BY_X_PATH = "//table[@id='ctl00_ContentPlaceHolder1_d_dettaglio']/tbody/tr"
+    ERROR_BOTTOM = "ctl00_ContentPlaceHolder1_pbOkErrScorm"
 
 
 settings = Settings()
